b660a2df7d131e7ba753b3c91a6321fc88375896
Natalia <nprokopenko@202-ecommerce.com>
2022-12-06 16:44:59 +0100

    Fix passive event listeners

--- slick/slick.js
+++ slick/slick.js
@@ -27,6 +27,20 @@
 
 }(function($) {
     'use strict';
+
+    // Before we do anything: One-fits-all solution to bind passive event listeners, if possible, to improve performance
+    jQuery.event.special.touchstart = {
+        setup: function(_, ns, handle) {
+            this.addEventListener('touchstart', handle, {passive: (ns.includes('noPreventDefault') ? true : false)});
+        }
+    };
+
+    jQuery.event.special.touchmove = {
+        setup: function(_, ns, handle) {
+            this.addEventListener('touchmove', handle, {passive: (ns.includes('noPreventDefault') ? true : false)});
+        }
+    };
+
     var Slick = window.Slick || {};
 
     Slick = (function() {
